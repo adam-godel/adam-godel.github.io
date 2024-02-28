bacb9ef53530295ae9144ada57b4f0b7f5a001ea
adam-godel <agodel@bu.edu>
2024-02-27 19:46:46 -0500

    Added JS button functionality and CSS hover mechanics

---
+++ script.js
@@ -0,0 +1,35 @@
+const project1Btn = document.getElementById("project1Btn");
+const project2Btn = document.getElementById("project2Btn");
+project1Btn.addEventListener("click", function () {
+    window.open("https://github.com");
+});
+project2Btn.addEventListener("click", function () {
+    window.open("https://github.com");
+});
+
+const linkedin = document.getElementById("linkedin-img");
+const github = document.getElementById("github-img");
+const email = document.getElementById("email");
+linkedin.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/adam-godel/");
+});
+github.addEventListener("click", function () {
+    window.open("https://github.com/adam-godel");
+});
+email.addEventListener("click", function () {
+    window.open("mailto:agodel@bu.edu", "_self");
+});
+
+hideFall23 = document.getElementById("hide-fall23");
+const project_hidden = document.getElementsByClassName("fall23");
+hideFall23.addEventListener("click", function () {
+    for (p of project_hidden) {
+        if (p.style.display == "none") {
+            p.style.display = "block";
+            hideFall23.textContent = "Hide";
+        } else {
+            p.style.display = "none";
+            hideFall23.textContent = "Show";
+        }
+    }
+});
